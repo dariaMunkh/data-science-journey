05249094e0dad7d7af37af7845b9cd096ea244ba
Daria <dariabayar0521@gmail.com>
2026-03-12 16:54:05 -0400

    Day 2: Python Course

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+# Madlibs game 
+# word game where you create a story 
+# by filling in blanks with random words
+
+# Take user input for blanks 
+adj1= input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+verb1 = input("Enter a verb: ")
+
+# Print statements
+print(f"I like to watch {adj1} stories")
+print(f"At my school I saw a(n) {noun1}")
+print(f"Today I went to {verb1}")
